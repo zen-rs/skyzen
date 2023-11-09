fb788f7690edd849b9fad9c5a0a2de596fbefed5
Lexo Liu <me@lexo.cool>
2023-11-09 17:11:23 +0800

    Handler shouldn't transform to `Middleware` directly

--- skyzen/src/middleware/mod.rs
+++ skyzen/src/middleware/mod.rs
@@ -1,21 +1,4 @@
 //! Utility for writing middleware.
-//!
-//! There're mutiple way to write a middleware for skyzen.
-//!
-//! # Tranform a handler to middleware
-//! ```rust
-//! use skyzen::middleware::into_middleware;
-//! async fn handler(){
-//!     
-//! }
-//!
-//! into_middleware(handler)
-//! ```
-//!
-//! This approach is very simple, but not flexible. So in some case, we should implement this trait directly.
-//!
-//! # Implement `Middleware` trait directly
-//!
 //! ```rust
 //! // An implement of timeout middleware
 //! use async_std::future::timeout;
@@ -32,46 +15,6 @@
 //! }
 //! ```
 mod error_handling;
-use std::marker::PhantomData;
-
-use async_trait::async_trait;
 pub use error_handling::ErrorHandlingMiddleware;
 pub use http_kit::middleware::Middleware;
 pub use http_kit::middleware::Next;
-use http_kit::Request;
-use http_kit::Response;
-use skyzen_core::Extractor;
-
-use crate::handler::Handler;
-
-struct IntoMiddleware<H: Handler<T>, T: Extractor> {
-    handler: H,
-    _marker: PhantomData<T>,
-}
-
-/// Transform handler to middleware.
-pub fn into_middleware<T: Extractor + Send + Sync>(handler: impl Handler<T>) -> impl Middleware {
-    IntoMiddleware::new(handler)
-}
-
-impl<H: Handler<T>, T: Extractor + Send + Sync> IntoMiddleware<H, T> {
-    /// Create an `IntoMiddleware` instance.
-    pub fn new(handler: H) -> Self {
-        Self {
-            handler,
-            _marker: PhantomData,
-        }
-    }
-}
-
-#[async_trait]
-impl<H: Handler<T>, T: Extractor + Send + Sync> Middleware for IntoMiddleware<H, T> {
-    async fn call_middleware(
-        &self,
-        request: &mut Request,
-        next: Next<'_>,
-    ) -> http_kit::Result<Response> {
-        self.handler.call_handler(request).await?;
-        next.run(request).await
-    }
-}
